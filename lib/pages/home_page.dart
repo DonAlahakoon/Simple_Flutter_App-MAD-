191c41af9871bb21a83055dad53e8d1e916beb83
DonAlahakoon <rashithaalahakoon@gmail.com>
2023-07-13 08:24:19 +0530

    Adding simple flutter app code

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple flutter app"),
+        centerTitle: true,
+      ),
+      body:Center(
+        child: Text("Hello World"),
+      )
+    );
+  }
+}
